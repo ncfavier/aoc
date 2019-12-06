9f16a5f49ad09dd512d6f36ece69b6676eae99b7
Naïm Favier <fnaim42@gmail.com>
2019-12-06 18:42:29 +0100

    Day 6

---
+++ 2019/6.hs
@@ -0,0 +1,15 @@
+import Control.Arrow
+import qualified Data.Map as M
+
+main = do
+    edges <- map (takeWhile (/= ')') &&& tail . dropWhile (/= ')')) . lines <$> readFile "input6"
+    let orbits = M.fromListWith (++) [(a, [b]) | (a, b) <- edges]
+        f n o = n + sum (map (f (n + 1)) (M.findWithDefault [] o orbits))
+    print $ f 0 "COM"
+    let parent = M.fromList [(b, a) | (a, b) <- edges]
+        pathToRoot n | Just p <- parent M.!? n = n:pathToRoot p
+                     | otherwise                = []
+        you = reverse $ pathToRoot "YOU"
+        san = reverse $ pathToRoot "SAN"
+        l = length $ takeWhile (uncurry (==)) $ zip you san
+    print $ length you + length san - 2*l - 2
