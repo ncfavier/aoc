0835d4be07bd6a8483f107140baad7e1781ef3d7
Naïm Favier <fnaim42@gmail.com>
2019-12-16 09:46:31 +0100

    Day 16

---
+++ src/2019/Day16.hs
@@ -0,0 +1,29 @@
+module Day16 where
+
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
+
+charToInt :: Char -> Int
+charToInt c = fromEnum c - fromEnum '0'
+
+pattern :: Int -> [Int]
+pattern i = tail $ cycle $ [0, 1, 0, -1] >>= replicate (i + 1)
+
+ones :: Int -> Int
+ones n = abs n `mod` 10
+
+phase :: [Int] -> [Int]
+phase l = [ones $ sum $ zipWith (*) l (pattern i) | i <- [0..pred (length l)]]
+
+phase' :: Vector Int -> Vector Int
+phase' = V.scanr1 f
+    where a `f` b = ones (a + b)
+
+main :: IO ()
+main = do
+    [input] <- lines <$> getContents
+    let offset = read $ take 7 input
+        l = map charToInt input
+    putStrLn $ concatMap show $ take 8 $ iterate phase l !! 100
+    let v = V.fromList $ drop offset $ concat $ replicate 10000 l
+    putStrLn $ concatMap show $ V.toList $ V.take 8 $ iterate phase' v !! 100
