34220e500ae1c0fa77bf6658213da83f1b710f34
Naïm Favier <fnaim42@gmail.com>
2019-12-20 18:33:01 +0100

    -

--- src/2019/18.hs
+++ src/2019/18.hs
@@ -30,14 +30,14 @@ main = do
                 d | isUpper d, toLower d `S.member` ks -> return (n, ks)
                 _ -> []
     print $ head [d | ((_, ks), d) <- bfs next (center, S.empty), length ks == nKeys]
-    let grid' = M.union (M.fromList $ (center, '#'):[(n, '#') | n <- neighbours center]) grid
+    let grid4 = M.union (M.fromList $ [(n, '#') | n <- center:neighbours center]) grid
         start4 = [left (up center), left (down center), right (down center), right (up center)]
         next4 (l, ps, ks) = do
             (p, ps') <- case l of
                 Just p -> return (p, ps)
                 Nothing -> [(p, delete p ps) | p <- ps]
             n <- neighbours p
-            case grid' ! n of
+            case grid4 ! n of
                 '.' -> return (Just n, ps', ks)
                 '@' -> return (Just n, ps', ks)
                 k | isLower k -> return (Nothing, n:ps', S.insert k ks)
