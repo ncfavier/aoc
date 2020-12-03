09248f9086281dd94576d3071180aa8080ac6a29
Naïm Favier <n@monade.li>
2020-12-03 07:12:06 +0100

    -

--- src/2020/Day03.hs
+++ src/2020/Day03.hs
@@ -6,9 +6,8 @@ import qualified Data.Map as Map
 import AOC
 
 main = do
-    input <- readInput
-    let (grid, width, height) = makeGrid input
-        countTrees slope = count (== '#') (go (0, 0)) where
+    (grid, width, height) <- makeGrid <$> readInput
+    let countTrees slope = count (== '#') (go (0, 0)) where
             go p@(x, y) | y >= height = []
                         | otherwise   = grid Map.! (x `mod` width, y):go (p `add` slope)
     print $ countTrees (3, 1)
