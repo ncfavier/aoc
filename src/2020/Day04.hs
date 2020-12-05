66a5479ddfdb5c2d90c2aca3c1e19775133a2821
Naïm Favier <n@monade.li>
2020-12-05 13:53:07 +0100

    existential types

--- src/2020/Day04.hs
+++ src/2020/Day04.hs
@@ -7,27 +7,30 @@ passports = passport `sepBy` newline where
     passport = field `endBy` oneOf " \n"
     field = (,) <$> many letterChar <* ":" <*> many (noneOf " \n")
 
+infix 1 :=>
+data FieldSpec = forall a. String :=> Parser a
+
+fieldsExist, formats :: [FieldSpec]
+fieldsExist = map (:=> skipMany anySingle)
+    ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 formats =
-    [ "byr" ==> numberInRange (1920, 2002)
-    , "iyr" ==> numberInRange (2010, 2020)
-    , "eyr" ==> numberInRange (2020, 2030)
-    , "hgt" ==> numberInRange (150, 193) *> "cm" <||> numberInRange (59, 76) *> "in"
-    , "hcl" ==> "#" *> count 6 hexDigitChar
-    , "ecl" ==> choice ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-    , "pid" ==> count 9 digitChar
-    ] where
-        infix 0 ==>
-        a ==> b = (a, () <$ b)
+    [ "byr" :=> numberInRange (1920, 2002)
+    , "iyr" :=> numberInRange (2010, 2020)
+    , "eyr" :=> numberInRange (2020, 2030)
+    , "hgt" :=> numberInRange (150, 193) *> "cm"
+           <||> numberInRange (59, 76) *> "in"
+    , "hcl" :=> "#" *> count 6 hexDigitChar
+    , "ecl" :=> choice ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    , "pid" :=> count 9 digitChar
+    ]
 
-fieldsExist = map (skipMany anySingle <$) formats
-
-validate :: [(String, Parser ())] -> [(String, String)] -> Bool
+validate :: [FieldSpec] -> [(String, String)] -> Bool
 validate formats passports = isJust do
-    for formats \(field, format) -> do
+    for_ formats \(field :=> format) -> do
         v <- lookup field passports
-        parseMaybe format v
+        () <$ parseMaybe format v
 
 main = do
     input <- parseInput passports
-    print $ howMany (validate fieldsExist) input
-    print $ howMany (validate formats) input
+    for [fieldsExist, formats] do
+        print . flip howMany input . validate
