cdade9dad18be5927b71f2f43efe5b333e2c2977
Naïm Favier <n@monade.li>
2020-12-06 09:37:42 +0100

    -

--- src/2016/Day11.hs
+++ src/2016/Day11.hs
@@ -9,49 +9,62 @@ import           Lens.Micro.Platform
 import AOC
 
 data UnitType = Chip | Generator
-              deriving (Eq, Ord, Show)
+              deriving (Eq, Ord)
 
 data Element = Strontium | Plutonium | Thulium | Ruthenium | Curium | Elerium | Dilithium
-             deriving (Eq, Ord, Show)
+             deriving (Eq, Ord)
 
-type Unit a = (a, UnitType)
+data Unit a = (:@:) { _element :: a
+                    , _unitType :: UnitType
+                    } deriving (Eq, Ord)
+
+makeLenses ''Unit
 
 data State a = State { _currentFloor :: Int
                      , _floors :: Vector (Set (Unit a))
-                     } deriving (Eq, Ord, Show)
+                     } deriving (Eq, Ord)
 
 makeLenses ''State
 
+initialState :: State Element
 initialState = State { _currentFloor = 0
                      , _floors = Vec.fromList $ map Set.fromList
-                         [ [(Strontium, Generator), (Strontium, Chip), (Plutonium, Generator), (Plutonium, Chip)]
-                         , [(Thulium, Generator), (Ruthenium, Generator), (Ruthenium, Chip), (Curium, Generator), (Curium, Chip)]
-                         , [(Thulium, Chip)]
+                         [ [Strontium :@: Generator, Strontium :@: Chip, Plutonium :@: Generator, Plutonium :@: Chip]
+                         , [Thulium :@: Generator, Ruthenium :@: Generator, Ruthenium :@: Chip, Curium :@: Generator, Curium :@: Chip]
+                         , [Thulium :@: Chip]
                          , []
                          ]
                      }
 
-extraUnits = Set.fromList [(Elerium, Generator), (Elerium, Chip), (Dilithium, Generator), (Dilithium, Chip)]
+extraUnits :: Set (Unit Element)
+extraUnits = Set.fromList [Elerium :@: Generator, Elerium :@: Chip, Dilithium :@: Generator, Dilithium :@: Chip]
 
-repState s@State{..} = s & floors . each . sets Set.map . _1 %~ fromJust . (`elemIndex` els)
-    where els = nub $ s ^.. floors . each . folded . _1
+-- Two states are considered equivalent if they only differ by a permutation of elements
+upToElementsPermutation :: Eq a => State a -> State Int
+upToElementsPermutation s@State{..} = s & floors . each . sets Set.map . element %~ fromJust . (`elemIndex` elementsOrder)
+    where elementsOrder = nub $ s ^.. floors . each . folded . element
 
-check floor = null [() | (_, Generator) <- units]
-            || and [(element, Generator) `Set.member` floor | (element, Chip) <- units]
+check :: Ord a => Set (Unit a) -> Bool
+check floor = null [() | _ :@: Generator <- units]
+            || and [element :@: Generator `Set.member` floor | element :@: Chip <- units]
             where units = Set.toList floor
 
+nextStates :: Ord a => State a -> [State a]
+nextStates State{..} = do
+    nextFloor <- [pred _currentFloor, succ _currentFloor]
+    guard (inRange (0, 3) nextFloor)
+    size <- [1, 2]
+    (moving, newCurrentFloor) <- pickSubset size (_floors Vec.! _currentFloor)
+    let newNextFloor = (_floors Vec.! nextFloor) <> moving
+    guard (check newCurrentFloor && check newNextFloor)
+    pure $ State nextFloor (_floors Vec.// [(_currentFloor, newCurrentFloor), (nextFloor, newNextFloor)])
+
+isDone :: State a -> Bool
 isDone State{..} = _currentFloor == 3 && all null (Vec.take 3 _floors)
 
-minSteps initialState = head [steps | (s, steps) <- states, isDone s] where
-    states = bfsOn repState nextStates initialState
-    nextStates State{..} = do
-        nextFloor <- [pred _currentFloor, succ _currentFloor]
-        guard (inRange (0, 3) nextFloor)
-        size <- [1, 2]
-        (moving, staying) <- pickSubset size (_floors Vec.! _currentFloor)
-        let newNextFloor = (_floors Vec.! nextFloor) <> moving
-        guard (check staying && check newNextFloor)
-        pure $ State nextFloor (_floors Vec.// [(_currentFloor, staying), (nextFloor, newNextFloor)])
+minSteps :: Ord a => State a -> Integer
+minSteps start = head [steps | (s, steps) <- states, isDone s]
+    where states = bfsOn upToElementsPermutation nextStates start
 
 main = do
     print $ minSteps initialState
