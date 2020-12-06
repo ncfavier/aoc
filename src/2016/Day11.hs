6ce1d1edaf27a3e3f03384af66b31534bf53bbeb
Naïm Favier <n@monade.li>
2020-12-06 09:18:12 +0100

    2016 day 11 part 2

--- src/2016/Day11.hs
+++ src/2016/Day11.hs
@@ -2,48 +2,57 @@ module Day11 where
 
 import           Data.Set (Set)
 import qualified Data.Set as Set
-import           Lens.Micro
+import           Data.Vector (Vector)
+import qualified Data.Vector as Vec
+import           Lens.Micro.Platform
 
 import AOC
 
 data UnitType = Chip | Generator
-              deriving (Eq, Ord)
+              deriving (Eq, Ord, Show)
 
 data Element = Strontium | Plutonium | Thulium | Ruthenium | Curium | Elerium | Dilithium
-             deriving (Eq, Ord)
+             deriving (Eq, Ord, Show)
 
-type Unit = (Element, UnitType)
+type Unit a = (a, UnitType)
 
-data State = State { currentFloor :: Int
-                   , floors :: [Set Unit]
-                   } deriving (Eq, Ord)
+data State a = State { _currentFloor :: Int
+                     , _floors :: Vector (Set (Unit a))
+                     } deriving (Eq, Ord, Show)
 
-initialState = State { currentFloor = 0
-                     , floors = map Set.fromList
-                         [ [(Strontium, Generator), (Strontium, Chip), (Plutonium, Generator), (Plutonium, Chip){-, (Elerium, Generator), (Elerium, Chip), (Dilithium, Generator), (Dilithium, Chip)-}]
+makeLenses ''State
+
+initialState = State { _currentFloor = 0
+                     , _floors = Vec.fromList $ map Set.fromList
+                         [ [(Strontium, Generator), (Strontium, Chip), (Plutonium, Generator), (Plutonium, Chip)]
                          , [(Thulium, Generator), (Ruthenium, Generator), (Ruthenium, Chip), (Curium, Generator), (Curium, Chip)]
                          , [(Thulium, Chip)]
                          , []
                          ]
                      }
 
+extraUnits = Set.fromList [(Elerium, Generator), (Elerium, Chip), (Dilithium, Generator), (Dilithium, Chip)]
+
+repState s@State{..} = s & floors . each . sets Set.map . _1 %~ fromJust . (`elemIndex` els)
+    where els = nub $ s ^.. floors . each . folded . _1
+
 check floor = null [() | (_, Generator) <- units]
             || and [(element, Generator) `Set.member` floor | (element, Chip) <- units]
             where units = Set.toList floor
 
-isDone State{..} = currentFloor == 3 && and [null f | f <- take 3 floors]
+isDone State{..} = _currentFloor == 3 && all null (Vec.take 3 _floors)
 
-minSteps = head [steps | (s, steps) <- states, isDone s] where
-    states = bfs nextStates initialState
+minSteps initialState = head [steps | (s, steps) <- states, isDone s] where
+    states = bfsOn repState nextStates initialState
     nextStates State{..} = do
-        nextFloor <- [pred currentFloor, succ currentFloor]
+        nextFloor <- [pred _currentFloor, succ _currentFloor]
         guard (inRange (0, 3) nextFloor)
         size <- [1, 2]
-        (moving, staying) <- pickSubset size (floors !! currentFloor)
-        let newNextFloor = (floors !! nextFloor) `Set.union` moving
+        (moving, staying) <- pickSubset size (_floors Vec.! _currentFloor)
+        let newNextFloor = (_floors Vec.! nextFloor) <> moving
         guard (check staying && check newNextFloor)
-        pure $ State nextFloor
-                     (floors & ix currentFloor .~ staying
-                             & ix nextFloor .~ newNextFloor)
+        pure $ State nextFloor (_floors Vec.// [(_currentFloor, staying), (nextFloor, newNextFloor)])
 
-main = print minSteps
+main = do
+    print $ minSteps initialState
+    print $ minSteps (initialState & floors . ix 0 <>~ extraUnits)
