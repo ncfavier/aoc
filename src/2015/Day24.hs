ec5020ee0265240944d02f28dc0f17bee398ec15
Naïm Favier <n@monade.li>
2021-11-26 23:54:38 +0100

    2015 day 24

---
+++ src/2015/Day24.hs
@@ -0,0 +1,14 @@
+module Day24 where
+
+import AOC
+
+main :: IO ()
+main = do
+  weights <- reverse <$> parseInput (eachLine number)
+  for_ [3, 4] \nGroups -> do
+    let goal = sum weights `div` nGroups
+        solve 0 _ = pure []
+        solve goal [] = empty
+        solve goal (w:ws) | w <= goal = (w:) <$> solve (goal - w) ws <|> solve goal ws
+                          | otherwise = solve goal ws
+    print $ product $ head $ sortOn (length &&& product) (solve goal weights :: [[Int]])
