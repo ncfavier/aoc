a294094a77c4015a3c29501dfc1907e6cef9de7f
Naïm Favier <n@monade.li>
2021-12-02 06:51:12 +0100

    pretty

--- src/2021/Day02.hs
+++ src/2021/Day02.hs
@@ -5,30 +5,27 @@ import AOC
 data Command = Forward | Down | Up
 
 format = eachLine do
-  d <- Forward <$ "forward" <|> Down <$ "down" <|> Up <$ "up"
-  " "
-  n <- number
-  pure (d, n)
+  c <- Forward <$ "forward" <|> Down <$ "down" <|> Up <$ "up"
+  n <- space *> number
+  pure (c, n)
 
 run1 Forward n = _2 += n
-run1 Down n = _1 += n
-run1 Up n = _1 -= n
+run1 Down    n = _1 += n
+run1 Up      n = _1 -= n
 
 run2 Forward n = do
   _2 += n
-  a <- use _3
-  _1 += a * n
+  aim <- use _3
+  _1 += aim * n
 run2 Down n = _3 += n
-run2 Up n = _3 -= n
+run2 Up   n = _3 -= n
 
 solve run start cs = evalState ?? start $ do
   traverse_ (uncurry run) cs
-  y <- use _1
-  x <- use _2
-  pure (x * y)
+  (*) <$> use _1 <*> use _2
 
 main :: IO ()
 main = do
-  cs <- parseInput format
-  print $ solve run1 (0, 0) cs
-  print $ solve run2 (0, 0, 0) cs
+  commands <- parseInput format
+  print $ solve run1 (0, 0) commands
+  print $ solve run2 (0, 0, 0) commands
