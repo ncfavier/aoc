29be53fb3e8123bd42e2402a7aede8264f7e5ae2
Naïm Favier <n@monade.li>
2021-12-08 21:30:42 +0100

    clean up day 8

--- src/2021/Day08.hs
+++ src/2021/Day08.hs
@@ -6,30 +6,22 @@ import Data.Map qualified as M
 import Data.Set qualified as S
 
 format = eachLine do
-  digits <- word `endBy` " "
-  "| "
-  outputs <- word `sepBy` " "
-  pure (map S.fromList digits, map S.fromList outputs)
+  let pat = S.fromList <$> word
+  (,) <$> pat `endBy` " " <* "| " <*> pat `sepBy` " "
 
-decode (digits, outputs) = r
+decode (digits, outputs) = (ds M.!) <$> outputs
   where
-    [one] = [d | d <- digits, length d == 2]
-    [eight] = [d | d <- digits, length d == 7]
-    [seven] = [d | d <- digits, length d == 3]
-    [four] = [d | d <- digits, length d == 4]
-    [three] = [d | d <- digits, length d == 5, seven `S.isSubsetOf` d]
-    l = four S.\\ one
-    [five] = [d | d <- digits, length d == 5, l `S.isSubsetOf` d]
-    [two] = [d | d <- digits, length d == 5, d /= five, d /= three]
-    [zero] = [d | d <- digits, length d == 6, not (l `S.isSubsetOf` d)]
-    [nine] = [d | d <- digits, length d == 6, one `S.isSubsetOf` d, d /= zero]
-    [six] = [d | d <- digits, length d == 6, d /= nine, d /= zero]
-    m = M.fromList [(zero,0),(one,1),(two,2),(three,3),(four,4),(five,5),(six,6),(seven,7),(eight,8),(nine,9)]
-    [a,b,c,d] = map (m M.!) outputs
-    r = a*1000+b*100+c*10+d
+    [d1, d7, d4, l51, l52, l53, l61, l62, l63, d8] = sortOn length digits
+    [d2, d3, d5, d0, d6, d9] = sortOn
+      (length &&& ((d4 S.\\ d1) `S.isSubsetOf`) &&& (d1 `S.isSubsetOf`))
+      [l51, l52, l53, l61, l62, l63]
+    ds = M.fromList [(d0,0),(d1,1),(d2,2),(d3,3),(d4,4),(d5,5),(d6,6),(d7,7),(d8,8),(d9,9)]
+
+toBase10 = foldl' (\a b -> a * 10 + b) 0
 
 main :: IO ()
 main = do
-  input <- parseInput format
-  print $ howMany ((`elem` [2,3,4,7]) . length) $ concatMap snd input
-  print $ sum $ decode <$> input
+  displays <- parseInput format
+  let outputs = decode <$> displays
+  print $ howMany (`elem` [1, 4, 7, 8]) $ concat outputs
+  print $ sum $ toBase10 <$> outputs
