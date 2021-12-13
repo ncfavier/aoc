616637ccfab6c75e2fa9a3173470375cfead258f
Naïm Favier <n@monade.li>
2021-12-13 06:50:31 +0100

    single pass

--- src/2021/Day13.hs
+++ src/2021/Day13.hs
@@ -14,9 +14,11 @@ doFold (direction, n) = S.map $ direction (\i -> if i > n then 2 * n - i else i)
 
 main :: IO ()
 main = do
-  (grid, folds@(f0:_)) <- parseInput format
+  (grid, f0:_) <- parseInput format
   print $ length $ doFold f0 grid
-  let folded = foldl (flip doFold) grid folds
+  let folded = S.map (f 40 *** f 6) grid
+      f n i = if even (i `div` (n + 1)) then r else n - 1 - r
+        where r = i `mod` (n + 1)
       ((xmin, ymin), (xmax, ymax)) = boundingBox folded
   for_ [ymin..ymax] \y -> do
     putStrLn [if (x, y) `S.member` folded then '#' else ' ' | x <- [xmin..xmax]]
