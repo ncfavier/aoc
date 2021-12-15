6e2b342ec5a129cd48d0ff835eaa112c7b2810f7
Naïm Favier <n@monade.li>
2021-12-15 12:33:08 +0100

    no need to pass width,height to solve

--- src/2021/Day15.hs
+++ src/2021/Day15.hs
@@ -7,8 +7,8 @@ import Data.Map qualified as M
 plus :: Int -> Int -> Int
 a `plus` b = (a + b - 1) `mod` 9 + 1
 
-solve :: Map (Int, Int) Int -> Int -> Int -> Int
-solve grid width height = fromJust $ lookup (width - 1, height - 1) (dijkstra step (0, 0))
+solve :: Map (Int, Int) Int -> Int
+solve grid = fromJust $ lookup (maximum (M.keys grid)) (dijkstra step (0, 0))
   where step = withNeighbours cardinal (,) grid
 
 main :: IO ()
@@ -17,5 +17,5 @@ main = do
   let grid5 = M.unions [ plus (i + j) <$> M.mapKeysMonotonic (+ offset) grid
                        | (i, j) <- range ((0, 0), (4, 4))
                        , let offset = (width * i, height * j)]
-  print $ solve grid width height
-  print $ solve grid5 (width * 5) (height * 5)
+  print $ solve grid
+  print $ solve grid5
