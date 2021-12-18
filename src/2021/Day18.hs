ef48fc8cb261bc96c790dc69a06a62eb83a0caca
Naïm Favier <n@monade.li>
2021-12-18 08:28:34 +0100

    clean up

--- src/2021/Day18.hs
+++ src/2021/Day18.hs
@@ -1,46 +1,39 @@
+{-# OPTIONS_GHC -Wno-missing-methods #-}
 module Day18 where
 
-import AOC hiding (left, right)
+import AOC
 
-import Data.Data.Lens
-import Data.Data
-
-data Snailfish = Pure { _it :: Int } | Pair { _left :: Snailfish, _right :: Snailfish } deriving (Data, Typeable)
-instance Show Snailfish where
-  show (Pure n) = show n
-  show (Pair a b) = "[" ++ show a ++ "," ++ show b ++ "]"
-
-makeLenses ''Snailfish
-
-format = eachLine snailfish
-  where
-    snailfish = pair <|> single
-    pair = Pair <$ "[" <*> snailfish <* "," <*> snailfish <* "]"
-    single = Pure <$> decimal
+data Snailfish = Pure Int | Pair Snailfish Snailfish
 
 magnitude (Pure n) = n
 magnitude (Pair a b) = 3 * magnitude a + 2 * magnitude b
 
-splitSingle n = Pair (Pure $ n `div` 2) (Pure $ (n + 1) `div` 2)
-explode p l r d (Pair (Pure a) (Pure b)) | d >= 4 = Just (l, r, p, a, b)
-explode p l r d (Pair a b) = explode (p . left) l (Just $ p . right) (d + 1) a <|> explode (p . right) (Just $ p . left) r (d + 1) b
-explode _ _ _ _ _ = Nothing
-split p (Pair a b) = split (p . left) a <|> split (p . right) b
-split p (Pure n) | n >= 10 = Just (p, splitSingle n)
-                 | otherwise = Nothing
-final n = case explode id Nothing Nothing 0 n of
-  Just (l, r, p, a, b) -> final $ n & fromMaybe ignored l . partsOf biplate . _last +~ a
-                                 & fromMaybe ignored r  . partsOf biplate . _head +~ b
-                                 & p .~ Pure 0
-  Nothing -> case split id n of
-    Just (p, ab) -> final $ n & p .~ ab
-    Nothing -> n
+snailfish = single <|> pair
+  where
+    single = Pure <$> decimal
+    pair = Pair <$ "[" <*> snailfish <* "," <*> snailfish <* "]"
 
-add a b = final $ Pair a b
+explode x = fmap (\(_, x, _) -> x) (go 0 x)
+  where
+    go d (Pair (Pure a) (Pure b)) | d >= 4 = Just (rightmost (+a), Pure 0, leftmost (+b))
+    go d (Pair a b) = (\(f, x, g) -> (f , Pair x (g b), id)) <$> go (d+1) a
+                  <|> (\(f, x, g) -> (id, Pair (f a) x, g )) <$> go (d+1) b
+    go _ _ = Nothing
+    leftmost f (Pure n) = Pure (f n)
+    leftmost f (Pair a b) = Pair (leftmost f a) b
+    rightmost f (Pure n) = Pure (f n)
+    rightmost f (Pair a b) = Pair a (rightmost f b)
+
+split (Pure n) | n >= 10 = Just $ Pair (Pure $ n `div` 2) (Pure $ (n + 1) `div` 2)
+               | otherwise = Nothing
+split (Pair a b) = flip Pair b <$> split a <|> Pair a <$> split b
+
+final x = maybe x final $ explode x <|> split x
+
+instance Num Snailfish where a + b = final (Pair a b)
 
 main :: IO ()
 main = do
-  input <- parseInput format
-  let number = foldl1 add input
-  print $ magnitude number
-  print $ maximum [magnitude (add a b) | a <- input, b <- input]
+  numbers <- parseInput (eachLine snailfish)
+  print $ magnitude $ foldl1 (+) numbers
+  print $ maximum [magnitude (a + b) | a <- numbers, b <- numbers]
