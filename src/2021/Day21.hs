d974b3f625be533e2a7904e0ac9b3d1e8c2f0647
Naïm Favier <n@monade.li>
2021-12-21 08:23:02 +0100

    names

--- src/2021/Day21.hs
+++ src/2021/Day21.hs
@@ -8,9 +8,9 @@ format = (,) <$ "Player 1 starting position: " <*> decimal <* newline
              <* "Player 2 starting position: " <*> decimal <* newline
 
 part1 :: Int -> Int -> Int
-part1 = go det 0 0 0
+part1 = go deterministic 0 0 0
   where
-    det = sum <$> chunksOf 3 (cycle [1..100])
+    deterministic = sum <$> chunksOf 3 (cycle [1..100])
     go (d:ds) !n !s1 !s2 !p1 !p2
       | s2 >= 1000 = s1 * n
       | otherwise = go ds (n + 3) s2 (s1 + p1') p2 p1'
@@ -19,11 +19,11 @@ part1 = go det 0 0 0
 part2 :: Int -> Int -> Int
 part2 = maximum . M.fromListWith (+) .* go 1 True 0 0
   where
-    nondet = M.assocs . counts $ sum <$> replicateM 3 [1..3]
+    dirac = M.assocs . counts $ sum <$> replicateM 3 [1..3]
     go !c !p !s1 !s2 !p1 !p2
       | s2 >= 21 = pure (p, c)
       | otherwise = do
-          (d, c') <- nondet
+          (d, c') <- dirac
           let p1' = (p1 + d) `mod1` 10
           go (c * c') (not p) s2 (s1 + p1') p2 p1'
 
