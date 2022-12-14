d5b2b68655fbd6080fdfd0679fe0dfbeb8813961
Naïm Favier <n@monade.li>
2022-12-14 11:00:23 +0100

    2022/14: speed up a bit

--- src/2022/Day14.hs
+++ src/2022/Day14.hs
@@ -12,11 +12,17 @@ main = do
   paths <- parseInput format
   let grid = M.fromList [(p, '#') | path <- paths, [a, b] <- sort <$> divvy 2 1 path, p <- range (a, b)]
       (_, (_, maxY)) = boundingBox grid
-      fall floor grid = go (500, 0) where
-        go p@(_, y) | if floor then p `M.member` grid else y > maxY = Nothing
-                    | Just p' <- move down <|> move (down + left) <|> move (down + right) = go p'
-                    | otherwise = Just $ M.insert p 'o' grid
-                    where move d | (p + d) `M.member` grid || snd (p + d) >= maxY + 2 = Nothing
-                                 | otherwise = Just (p + d)
+      start = (500, 0)
+      fall floor grid
+        | floor && start `M.member` grid = Nothing
+        | otherwise = go start
+        where go p@(_, y)
+                | not floor && y > maxY = Nothing
+                | Just p' <- move down <|> move (down + left) <|> move (down + right) = go p'
+                | otherwise = Just (M.insert p 'o' grid)
+                where move d
+                        | y' >= maxY + 2 || p' `M.member` grid = Nothing
+                        | otherwise = Just p'
+                        where p'@(_, y') = p + d
   for_ [False, True] \ floor ->
     print $ length $ tail $ iterateMaybe (fall floor) grid
