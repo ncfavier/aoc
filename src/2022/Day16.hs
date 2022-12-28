df0e884a696cdd33f45c9ffcc0a8078805ab4c8f
Naïm Favier <n@monade.li>
2022-12-28 12:01:47 +0100

    2022/16, still too slow

--- src/2022/Day16.hs
+++ src/2022/Day16.hs
@@ -14,20 +14,21 @@ format = M.fromList <$> eachLine valve where
 
 maximumPressure valves time workers = {-[h | (s@(h@(t, _, _)), _) <- steps] -} head [time * sum pressures - p | ((t, _, _), p) <- steps, t == time]
   where
-  distances = flip M.mapWithKey valves \ v _ -> M.fromList $ bfs (\v -> snd (valves M.! v)) [v]
+  distances = flip M.mapWithKey valves \ v _ -> M.fromList $ bfs (\ v -> snd (valves M.! v)) [v]
   pressures = M.filter (> 0) . fmap fst $ valves
-  next (t, agents, open) = do
-    let goals = pressures `M.withoutKeys` open
-        cost = sum goals
-        act (Idle v) = do
-          asum ([act (Moving g (distances M.! v M.! g)) | g <- M.keys goals] <> [pure (Done 0) | M.null goals])
-        act (Moving g 0) = Idle g <$ modify (S.insert g)
+  next (t, agents, closed) = do
+    let cost = sum closed + sum [pressures M.! g | Moving g _ <- agents]
+        act (Idle v) = get >>= \case
+          closed | M.null closed -> pure (Done 0)
+                 | otherwise -> asum [modify (M.delete g) *> act (Moving g (distances M.! v M.! g)) | g <- candidates]
+                 where candidates = [g | g <- M.keys closed, all (\ g' -> pressures M.! g' <= pressures M.! g || distances M.! v M.! g' >= distances M.! v M.! g) (M.keys closed)]
+        act (Moving g 0) = pure (Idle g)
         act (Moving g c) = pure (Moving g (pred c))
         act (Done c) = pure (Done (succ c))
-    (agents', open') <- runStateT (traverse act agents) open
-    pure ((succ t, agents', open'), cost)
+    (agents', closed') <- runStateT (traverse act agents) closed
+    pure ((succ t, agents', closed'), cost)
   rep (_, agents, open) = (sort agents, open)
-  steps = dijkstraOn rep next [(0, replicate workers (Idle "AA"), S.empty)]
+  steps = dijkstraOn rep next [(0, replicate workers (Idle "AA"), pressures)]
 
 main :: IO ()
 main = do
