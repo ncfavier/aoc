1fac14298b0d20be620a553435b79b7598557279
Naïm Favier <n@monade.li>
2023-12-10 17:48:24 +0100

    day 1 part 2

--- src/2023/Day01.hs
+++ src/2023/Day01.hs
@@ -2,9 +2,11 @@ module Day01 where
 
 import AOC
 
-format = eachLine takeRest
-
 main :: IO ()
 main = do
-  input <- sum . map (read . sequence [head, last] .  (filter isDigit)) <$> parseInput format
-  print input
+  input <- parseInput (eachLine takeRest)
+  print $ sum $ map (read . sequence [head, last] . filter isDigit) $ input
+  let strings = ([(show i, head (show i)) | i <- [1..9]] ++ [("one", '1'), ("two", '2'), ("three", '3'), ("four", '4'), ("five", '5'), ("six", '6'), ("seven", '7'), ("eight", '8'), ("nine", '9')])
+      search strings s = [b | (a, b) <- strings, a `isPrefixOf` s] <> search strings (tail s)
+      number = read . sequence [head . search strings, head . search (map (first reverse) strings) . reverse]
+  print $ sum $ map number input
