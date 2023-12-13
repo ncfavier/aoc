04e830b978823e2c0d44b6bef3302ac25ead0553
Naïm Favier <n@monade.li>
2023-12-13 13:58:33 +0100

    day 12 part 2
    using numbers instead of [()] works somewhat better... 🤦

--- src/2023/Day12.hs
+++ src/2023/Day12.hs
@@ -5,19 +5,19 @@ import AOC
 format = eachLine do (,) <$> takeWhileP (/= ' ') <* space1 <*> number `sepBy` ","
 
 arrangements = memoFix \ go -> \case
-  ('?':xs, ns) -> go ('.':xs, ns) <> go ('#':xs, ns)
+  ('?':xs, ns) -> go ('.':xs, ns) + go ('#':xs, ns)
   ('.':xs, ns) -> go (xs, ns)
   (xs@('#':_), (n:ns))
     | length xs == n && all (/= '.') (take n xs) -> go (drop n xs, ns)
     | length xs > n && all (/= '.') (take n xs) && (xs !! n) /= '#' -> go (drop (n + 1) xs, ns)
-    | otherwise -> []
-  ('#':xs, []) -> []
-  ([], []) -> [()]
-  ([], _) -> []
+    | otherwise -> 0
+  ('#':_, []) -> 0
+  ([], []) -> 1
+  ([], _) -> 0
 
 unfold (xs, ns) = (intercalate "?" (replicate 5 xs), concat (replicate 5 ns))
 
 main = do
   input <- parseInput format
-  print $ sum $ map (length . arrangements) input
-  print $ sum $ map (length . arrangements . unfold) input
+  print $ sum $ map arrangements input
+  print $ sum $ map (arrangements . unfold) input
--- src/2023/Day13.hs
+++ src/2023/Day13.hs
@@ -8,10 +8,10 @@ folds = go [] where
   go past [] = [(past, [])]
   go past (x:xs) = (past, x:xs):go (x:past) xs
 
-reflection xs = head $ (map (100 *) (go xs)) ++ go (transpose xs) where
+reflection xs = head $ map (100 *) (go xs) ++ go (transpose xs) where
   go xs = [length past | (past, future) <- folds xs, notNull past, notNull future, and (zipWith (==) past future)]
 
-smudge xs = head $ (map (100 *) (go xs)) ++ go (transpose xs) where
+smudge xs = head $ map (100 *) (go xs) ++ go (transpose xs) where
   go xs = [length past | (past, future) <- folds xs, notNull past, notNull future, howMany id (concat (zipWith (zipWith xor) past future)) == 1]
 
 main = do
